e23518ea473f6ac48a83eb428dff5e8b33e2136e
Kenneth O'Neill <kenneth.oneill@gmail.com>
2018-07-27 22:47:29 +0100

    Now creating directories

--- pgit.py
+++ pgit.py
@@ -1,14 +1,20 @@
 import argparse
+import os
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     sub_parsers = parser.add_subparsers(dest='command', metavar='command')
     sub_parsers.required = True
-    sub_parsers.add_parser('init', help="Initialise a new repo")
+    sub_parsers.add_parser('init', help="Initialise empty git repository in current directory")
 
     args = parser.parse_args()
     if args.command == 'init':
-        print ("New repo initialised")
+        git_dir = os.path.join(os.getcwd(), ".git")
+        os.mkdir(git_dir)
+        os.mkdir(os.path.join(git_dir, "refs"))
+        os.mkdir(os.path.join(git_dir, "objects"))
+        open(os.path.join(git_dir, "HEAD"), "w")
+        print ("Initialised empty git repository in {}". format (git_dir))
 
 
